8c9cc9dc8e2845c18ba0d68c56fde5eb23758f46
phoenixfusionx <jameslovett502@gmail.com>
2025-06-19 10:34:59 -0500

    fix resolve payment issue

--- lib/utils.ts
+++ lib/utils.ts
@@ -83,7 +83,6 @@ export function formatPrice(priceInCents: string) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-    // Use minimumFractionDigits to handle cases like $59.00 -> $59
     minimumFractionDigits: dollars % 1 !== 0 ? 2 : 0,
   }).format(dollars);
 }
